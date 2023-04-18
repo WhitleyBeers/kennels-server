baf055d5e7bf371cf75ea003283802066eb85af5
Whitley Beers <whitleybeers@gmail.com>
2023-04-18 18:50:23 -0500

    added ability to create employees and customers, verified in postman

--- request_handler.py
+++ request_handler.py
@@ -2,8 +2,8 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from views import get_all_animals, get_single_animal, create_animal
 from views import get_single_location, get_all_locations, create_location
-from views import get_all_employees, get_single_employee
-from views import get_all_customers, get_single_customer
+from views import get_all_employees, get_single_employee, create_employee
+from views import get_all_customers, get_single_customer, create_customer
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -139,6 +139,14 @@ class HandleRequests(BaseHTTPRequestHandler):
         if resource == "locations":
             new_location = create_location(post_body)
             self.wfile.write(json.dumps(new_location).encode())
+
+        if resource == "employees":
+            new_employee = create_employee(post_body)
+            self.wfile.write(json.dumps(new_employee))
+
+        if resource == "customers":
+            new_customer = create_customer(post_body)
+            self.wfile.write(json.dumps(new_customer))
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
--- views/employee_requests.py
+++ views/employee_requests.py
@@ -26,3 +26,13 @@ def get_single_employee(id):
             requested_employee = employee
 
     return requested_employee
+
+
+def create_employee(employee):
+    """creates a new employee
+    """
+    max_id = EMPLOYEES[-1]["id"]
+    new_id = max_id + 1
+    employee["id"] = new_id
+    EMPLOYEES.append(employee)
+    return employee

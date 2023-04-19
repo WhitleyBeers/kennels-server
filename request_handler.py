d0cb6e7ddc95d6c4903e167b89590661ed871040
Whitley Beers <whitleybeers@gmail.com>
2023-04-18 20:15:35 -0500

    added update animal and tested in postman

--- request_handler.py
+++ request_handler.py
@@ -4,6 +4,8 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 from views import get_all_animals, get_single_animal, create_animal, delete_animal
 
+from views import update_animal
+
 from views import get_single_location, get_all_locations, create_location
 
 from views import delete_location
@@ -165,7 +167,20 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_PUT(self):
         """Handles PUT requests to the server
         """
-        self.do_POST()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
     def do_DELETE(self):
         """handles DELETE requests to the server
--- views/__init__.py
+++ views/__init__.py
@@ -1,4 +1,5 @@
 from .animal_requests import get_all_animals, get_single_animal, create_animal, delete_animal
+from .animal_requests import update_animal
 from .location_requests import get_all_locations, get_single_location
 from .location_requests import create_location, delete_location
 from .employee_requests import get_all_employees, get_single_employee

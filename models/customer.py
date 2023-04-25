aea9fe2e1674a515a6d898795341e3336c950d6f
Whitley Beers <whitleybeers@gmail.com>
2023-04-24 19:31:25 -0500

    get customer by email and get animal by location id added, verified in postman

--- models/customer.py
+++ models/customer.py
@@ -1,7 +1,7 @@
 class Customer():
     """customer class
     """
-    def __init__(self, id, name, address, email, password):
+    def __init__(self, id, name, address, email = "", password = ""):
         self.id = id
         self.name = name
         self.address = address
